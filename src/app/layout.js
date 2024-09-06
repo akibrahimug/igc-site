2f5284c1e0a37ed35a51ff0877e9753e981ef2d4
GeoXhacker <mukisageophrey@gmail.com>
2024-09-06 14:56:54 +0300

    feat: migrated to project to app router and init storyblok setup

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import "./global.css";
+import Navigation from "@/components/Navigation";
+import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+import StoryblokProvider from "@/components/StoryblokProvider";
+
+storyblokInit({
+	accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
+	use: [apiPlugin],
+	apiOptions: {
+		region: "eu",
+	},
+});
+
+export default function RootLayout({ children }) {
+	return (
+		<StoryblokProvider>
+			<html lang="en">
+				<body>
+					<Navigation />
+					{children}
+				</body>
+			</html>
+		</StoryblokProvider>
+	);
+}
