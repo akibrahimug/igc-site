45e4ae32d746d107f927b442fe1bd221fc2ae963
GeoXhacker <mukisageophrey@gmail.com>
2024-10-03 09:25:30 +0300

    feat: add menuItem

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -139,3 +139,63 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+const MenuItem = ({ item }) => {
+  const pathname = usePathname();
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const toggleSubMenu = () => {
+    setSubMenuOpen(!subMenuOpen);
+  };
+
+  return (
+    <div className="">
+      {item.submenu ? (
+        <>
+          <button
+            onClick={toggleSubMenu}
+            className={`flex flex-row items-center p-2 rounded-lg hover-bg-zinc-100 w-full justify-between hover:opacity-50 ${
+              pathname.includes(item.path) ? 'bg-opacity-50' : ''
+            }`}
+          >
+            <div className="flex flex-row space-x-4 items-center">
+              {item.icon}
+              <span className="font-semibold text-xl  flex">{item.title}</span>
+            </div>
+
+            <div className={`${subMenuOpen ? 'rotate-180' : ''} flex`}>
+              <ChevronDown width="24" height="24" />
+            </div>
+          </button>
+
+          {subMenuOpen && (
+            <div className="my-2 ml-12 flex flex-col space-y-4">
+              {item.subMenuItems?.map((subItem, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    href={subItem.path}
+                    className={`${
+                      subItem.path === pathname ? 'font-bold' : ''
+                    }`}
+                  >
+                    <span>{subItem.title}</span>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
+        </>
+      ) : (
+        <Link
+          href={item.path}
+          className={`flex flex-row space-x-4 items-center p-2 rounded-lg hover:bg-opacity-20 ${
+            item.path === pathname ? 'bg-opacity-20' : ''
+          }`}
+        >
+          {item.icon}
+          <span className="font-semibold text-xl flex">{item.title}</span>
+        </Link>
+      )}
+    </div>
+  );
+};
