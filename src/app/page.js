0941c14d4b1dc4ca15c73e84bfcbaab04d8c8686
GeoXhacker <mukisageophrey@gmail.com>
2024-10-04 11:36:04 +0300

    feat: add Gallery component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
+"use client"
 import Hero from "@/components/Hero";
 import NewsSection from "@/components/NewsSection";
 import ScrollIndicator from "@/components/Indicators";
 import { getStoryblokApi } from "@storyblok/react/rsc";
+import Gallery from "@/components/Gallery";
 
-export const metadata = {
-	title: "IGC Home",
-}
 export default async function Home() {
 	const {	data: { story } } = await getStoryblokApi().get(`cdn/stories/igc/home`, { version: "draft" });
 
@@ -13,6 +12,7 @@ export default async function Home() {
 		<>
 			<div className="h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth relative z-0">
 				<Hero blok={story?.content} />
+				<Gallery blok={story?.content}/>
 				<NewsSection />
 			</div>
 			<ScrollIndicator />
