1bdb73c45ab601c3e6a11cfeb3452f55b727516a
GeoXhacker <mukisageophrey@gmail.com>
2024-11-11 13:49:25 +0300

    feat: updated with template 3

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,3 +1,4 @@
+import Template3 from '@/components/template-3'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -11,32 +12,9 @@ export default function Portfolio() {
     { title: "KAMPALA DISASTERS", href:"/portfolio/",image: "https://igcfashion.africa/images/folio/sample/AW19/aw2.jpg" },
   ]
 
+  const image = "https://igcfashion.africa/images/folio/sample/AW19/aw4.jpg"
+  const title = "PORTFOLIO"
   return (
-    <div className="bg-black text-white p-4 md:mx-32 lg:mx-48">      
-      <h1 className="text-5xl md:text-6xl font-bold mb-6 text-center">PORTFOLIO</h1>
-      
-      <p className="mb-8 max-w-lg text-justify">
-        By transforming waste materials into unique products, we minimise environmental impact and avoid intensive manufacturing processes. We collaborate with local artists to amplify voices from regions disproportionately affected by climate change. Through education and promotion of circular fashion practices, we're fostering a generation of conscious 'wastepreneurs'.
-      </p>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24">
-        {projects.map((project, index) => (
-          <Link href={project.href} key={index}>
-            <div key={index} className="relative h-[500px] overflow-hidden">
-              <Image
-                src={project.image}
-                alt={project.title}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg"
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                <h2 className="text-white text-2xl font-bold text-center px-4">{project.title}</h2>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-      </div>
+    <Template3 projects={projects} image={image} title={title}/>
   )
 }
