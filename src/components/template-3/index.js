89b3dda24dcdb297a601bc5cc7bf40641740e8aa
GeoXhacker <mukisageophrey@gmail.com>
2024-11-30 14:55:51 +0300

    style: add transitions on hover

--- src/components/template-3/index.js
+++ src/components/template-3/index.js
@@ -11,23 +11,22 @@ export default function Template3({projects, title, description, image }) {
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-12 p-4">
         {projects.map((project, index) => (
           <Link href={project.href} key={index}>
-            <div key={index} className="relative overflow-hidden">
+            <div key={index} className="relative overflow-hidden group">
               <Image
                 src={project.image}
                 alt={project.title}
-                // layout="fill"
                 width={600}
-								height={1200}
+                height={1200}
                 objectFit="cover"
-                className="rounded-lg"
+                className="rounded-lg transition-transform duration-500 group-hover:scale-110"
               />
-              <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center">
-                <h2 className="text-white text-2xl font-bold text-center px-4">{project.title}</h2>
+              <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center transition-all duration-500 group-hover:bg-opacity-10">
+                <h2 className="text-white text-2xl font-bold text-center px-4 transform transition-all duration-500 group-hover:scale-105 group-hover:-translate-y-2">{project.title}</h2>
               </div>
             </div>
           </Link>
         ))}
       </div>
-      </div>
+    </div>
   )
 }
